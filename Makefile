dd5ca3b3aa68adab9765bf437eae3ac86c0f3a16
o.kravtsov <o.kravtsov@russiainit.ru>
2018-06-11 13:33:34 +0400

    init

---
+++ Makefile
@@ -0,0 +1,83 @@
+.PHONY: all dist build-server package test clean run update-dependencies-after-release
+
+GOPATH ?= $(GOPATH:)
+GOFLAGS ?= $(GOFLAGS:)
+BUILD_NUMBER ?= $(BUILD_NUMBER:)
+BUILD_DATE = $(shell date -u)
+BUILD_HASH = $(shell git rev-parse HEAD)
+
+GO=go
+
+ifeq ($(BUILD_NUMBER),)
+	BUILD_NUMBER := dev
+endif
+
+DIST_ROOT=dist
+DIST_PATH=$(DIST_ROOT)/mattermost-push-proxy
+
+TESTS=.
+
+all: dist
+
+dist: | build-server test package
+
+update-dependencies-after-release:
+	@echo Run this to updated the go lang dependencies after a major release
+	glide up
+
+build-server: | .prebuild
+	@echo Building proxy push server
+
+	rm -Rf $(DIST_ROOT)
+	$(GO) clean $(GOFLAGS) -i ./...
+
+	@echo GOFMT
+	$(eval GOFMT_OUTPUT := $(shell gofmt -d -s server/ main.go 2>&1))
+	@echo "$(GOFMT_OUTPUT)"
+	@if [ ! "$(GOFMT_OUTPUT)" ]; then \
+		echo "gofmt sucess"; \
+	else \
+		echo "gofmt failure"; \
+		exit 1; \
+	fi
+
+	$(GO) build $(GOFLAGS) ./...
+	$(GO) install $(GOFLAGS) ./...
+
+package:
+	@ echo Packaging push proxy
+
+	mkdir -p $(DIST_PATH)/bin
+	cp $(GOPATH)/bin/mattermost-push-proxy $(DIST_PATH)/bin
+
+	cp -RL config $(DIST_PATH)/config
+	touch $(DIST_PATH)/config/build.txt
+	echo $(BUILD_NUMBER) | tee -a $(DIST_PATH)/config/build.txt
+
+	mkdir -p $(DIST_PATH)/logs
+
+	cp LICENSE.txt $(DIST_PATH)
+	cp NOTICE.txt $(DIST_PATH)
+	cp README.md $(DIST_PATH)
+
+	tar -C dist -czf $(DIST_PATH).tar.gz mattermost-push-proxy
+
+test:
+	$(GO) test $(GOFLAGS) -run=$(TESTS) -test.v -test.timeout=180s ./server || exit 1
+
+clean:
+	@echo Cleaning
+	rm -Rf $(DIST_ROOT)
+	go clean $(GOFLAGS) -i ./...
+
+	rm -f .prebuild
+
+.prebuild:
+	@echo Preparation for running go code
+	go get $(GOFLAGS) github.com/Masterminds/glide
+
+	touch $@
+
+run: .prebuild
+	@echo Starting go web server
+	$(GO) run $(GOFLAGS) main.go
---
+++ server/android_notification_server.go
@@ -0,0 +1,97 @@
+// Copyright (c) 2015 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package server
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alexjlockwood/gcm"
+	"github.com/kyokomi/emoji"
+)
+
+type AndroidNotificationServer struct {
+	AndroidPushSettings AndroidPushSettings
+}
+
+func NewAndroideNotificationServer(settings AndroidPushSettings) NotificationServer {
+	return &AndroidNotificationServer{AndroidPushSettings: settings}
+}
+
+func (me *AndroidNotificationServer) Initialize() bool {
+	LogInfo(fmt.Sprintf("Initializing Android notificaiton server for type=%v", me.AndroidPushSettings.Type))
+
+	if len(me.AndroidPushSettings.AndroidApiKey) == 0 {
+		LogError("Android push notifications not configured.  Mssing AndroidApiKey.")
+		return false
+	}
+
+	return true
+}
+
+func (me *AndroidNotificationServer) SendNotification(msg *PushNotification) PushResponse {
+	var data map[string]interface{}
+	if msg.Type == PUSH_TYPE_CLEAR {
+		data = map[string]interface{}{
+			"type":              PUSH_TYPE_CLEAR,
+			"badge":             msg.Badge,
+			"channel_id":        msg.ChannelId,
+			"team_id":           msg.TeamId,
+			"sender_id":         msg.SenderId,
+			"override_username": msg.OverrideUsername,
+			"override_icon_url": msg.OverrideIconUrl,
+			"from_webhook":      msg.FromWebhook,
+		}
+	} else {
+		data = map[string]interface{}{
+			"type":              PUSH_TYPE_MESSAGE,
+			"badge":             msg.Badge,
+			"message":           emoji.Sprint(msg.Message),
+			"channel_id":        msg.ChannelId,
+			"channel_name":      msg.ChannelName,
+			"team_id":           msg.TeamId,
+			"post_id":           msg.PostId,
+			"root_id":           msg.RootId,
+			"sender_id":         msg.SenderId,
+			"override_username": msg.OverrideUsername,
+			"override_icon_url": msg.OverrideIconUrl,
+			"from_webhook":      msg.FromWebhook,
+		}
+	}
+
+	regIDs := []string{msg.DeviceId}
+	gcmMsg := gcm.NewMessage(data, regIDs...)
+
+	sender := &gcm.Sender{
+		ApiKey: me.AndroidPushSettings.AndroidApiKey,
+		Http:   httpClient,
+	}
+
+	if len(me.AndroidPushSettings.AndroidApiKey) > 0 {
+		LogInfo(fmt.Sprintf("Sending android push notification for type=%v", me.AndroidPushSettings.Type))
+		start := time.Now()
+		resp, err := sender.Send(gcmMsg, 2)
+		observeGCMResponse(time.Since(start).Seconds())
+
+		if err != nil {
+			LogError(fmt.Sprintf("Failed to send GCM push sid=%v did=%v err=%v type=%v", msg.ServerId, msg.DeviceId, err, me.AndroidPushSettings.Type))
+			incrementFailure(me.AndroidPushSettings.Type)
+			return NewErrorPushResponse("unknown transport error")
+		}
+
+		if resp.Failure > 0 {
+			if len(resp.Results) > 0 && (resp.Results[0].Error == "InvalidRegistration" || resp.Results[0].Error == "NotRegistered") {
+				LogInfo(fmt.Sprintf("Android response failure sending remove code: %v type=%v", resp, me.AndroidPushSettings.Type))
+				incrementRemoval(me.AndroidPushSettings.Type)
+				return NewRemovePushResponse()
+			}
+			LogError(fmt.Sprintf("Android response failure: %v type=%v", resp, me.AndroidPushSettings.Type))
+			incrementFailure(me.AndroidPushSettings.Type)
+			return NewErrorPushResponse("unknown send response error")
+		}
+	}
+
+	incrementSuccess(me.AndroidPushSettings.Type)
+	return NewOkPushResponse()
+}
